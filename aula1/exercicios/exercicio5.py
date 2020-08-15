16c2ee985246b0449b99981dc78ea3abee190175
Wenderson Francelino dos Santos <wendyfranca895@gmail.com>
2020-08-15 19:54:35 -0300

    blz

--- aula1/exercicios/exercicio5.py
+++ aula1/exercicios/exercicio5.py
@@ -2,3 +2,6 @@
 # depois o sobrenome do usuário.
 # Escreva na tela primeiro o sobrenome e depois
 # o nome usando o f-string
+nome =str(input("escreva nome "  ,))
+sobrenome =str(input("escreva o sobrenome " ))
+print(f"{nome}  {sobrenome}")
--- aula1/exercicios/exercicio6.py
+++ aula1/exercicios/exercicio6.py
@@ -3,3 +3,8 @@
 # o preço do produto
 # a quantidade
 # e mostre usando o f-string
+nome_produto =input("nome do produto ")
+preco =float(input("preço do produto "))
+quantidade =float(input("quantidade "))
+
+print(f"nome do produto:{nome_produto} preco do produto:{preco} quantidade: {quantidade}")
